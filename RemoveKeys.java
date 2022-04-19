b2823c5d22dbf2bdd0c71e1801aff652a5e96206
Sravanthi1108 <101916647+Sravanthi1108@users.noreply.github.com>
2022-04-19 11:25:42 +0530

    file is added with changes

--- RemoveKeys.java
+++ RemoveKeys.java
@@ -11,27 +11,47 @@ public class RemoveKeys {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 
-		HashMap<Integer, String>hashMap=new HashMap<Integer, String>();
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));int n = Integer.parseInt(bufferedReader.readLine());
+		HashMap<Integer, String> hashMap=new HashMap<Integer, String>();
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bufferedReader.readLine());
 		for(int i=0;i<n;i++){
 		  hashMap.put(Integer.parseInt(bufferedReader.readLine()), bufferedReader.readLine());
 		}
-		getvalues(hashMap);
+		int size=0;
+		size=sizeOfResultandHashMap(hashMap);
+		System.out.println(size);
 		}
-		public static void getvalues(HashMap<Integer, String> hashMap) {
+		public static int sizeOfResultandHashMap(HashMap<Integer, String> hashMap) {
 		int count=0;
-		Iterator<Integer>itr=hashMap.keySet().iterator();
-		while(itr.hasNext())
+		Set<Integer>  keyValues=hashMap.keySet();
+		Iterator<Integer> iterator=keyValues.iterator();
+
+
+		//Iterator<Integer> itr=hashMap.keySet().iterator();//set interface
+		HashMap<Integer,String> hashMap1=new HashMap<>();
+		while(iterator.hasNext())
 		{
-		int n=itr.next();
-		if(n%4!=0)
+			//HashMap<Integer,String> hashMap1=new HashMap<>();
+		int key=iterator.next();
+		if(key%4==0)
 		{
+			
+			//hashMap.remove(key, hashMap.get(key));
+		}
+		else
+		{
+			hashMap1.put(key, hashMap1.get(key));
 		count++;
 		}
 		}
-		System.out.println(count);
+		
+		System.out.println(hashMap1);
+		
+		return count;
+		//System.out.println(count);
 		}
 
+		
 		
 	}
 
